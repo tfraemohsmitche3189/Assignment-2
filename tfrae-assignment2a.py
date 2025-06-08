e9608cf3a56de6b0f0478110bfcb7d2eabe8cb38
Talea Fraemohs-Mitchell <tfraemohsmitche3189@conestogac.on.ca>
2025-06-08 14:12:56 -0400

    Psuedocode written

---
+++ tfrae-assignment2a.py
@@ -0,0 +1,28 @@
+# Project name: Assignment 2a "Arnold's Amazing Eats"
+# Author: Talea Fraemohs-Mitchell
+# Date created: 06-08-25
+# Last edited: 06-08-25
+
+# Display welcome screen here, explain how to use program
+
+# Get the following information (Display prompts, recieve input). Don't sanitize these inputs.
+
+# First name
+# Last name
+# Street address
+# City
+# Province
+# Postal Code
+# Phone number
+# Special instructions for delivery
+
+# Menu: 2 options available, item and price
+# How many?
+
+# Print order confirmation, require Y/N in a loop (sanitize this input)
+
+# Ask if student (sanitize this input) and if so apply 10% discount
+
+# Add sales tax 13%
+
+# Print recipt including: Food ordered, quantity, price, student discount if applicable, sales tax
